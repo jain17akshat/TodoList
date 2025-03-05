f6d71cd1eec40a8f95d57f7b580415804f7820c8
Akshat Jain <161316297+jain17akshat@users.noreply.github.com>
2025-03-05 17:50:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./Context/AuthContext";  // Ensure the correct path
+import { AuthProvider } from "./Context/AuthContext";  
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>  {/* Wrap with AuthProvider */}
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
