21f1aaaa49e156a4f016e2c87dfb06d76806c83c
jain17akshat <161316297+jain17akshat@users.noreply.github.com>
2025-03-14 15:04:14 +0530

    Refactor TodoForm component to fix input value assignment

--- src/assets/components/TodoForm.jsx
+++ src/assets/components/TodoForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useTodo } from "../../Context/TodoContext";  
     function TodoForm() {
-    const [todo,setTodo]=useState([])
+    const [todo,setTodo]=useState("")
 const {addTodo}=useTodo()
 const add=(e)=>{
     e.preventDefault()
@@ -16,7 +16,7 @@ const add=(e)=>{
                     type="text"
                     placeholder="Write Todo..."
                     className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-                   vale={todo}
+                   value={todo}
                    onChange={(e)=>setTodo(e.target.value)}
                 
                 />
